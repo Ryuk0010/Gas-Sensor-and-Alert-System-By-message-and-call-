f86fa3e345fa6ab6b960408d24427aa2e591bffc
Mousam Bachhar <142692479+Ryuk0010@users.noreply.github.com>
2024-10-20 20:58:43 +0530

    Update Arduino.ino

--- Arduino.ino
+++ Arduino.ino
@@ -1,80 +1,104 @@
-#include <SoftwareSerial.h>   
-SoftwareSerial SIM900A(10,11); // SoftSerial( RX , TX );   
+#include <SoftwareSerial.h>
 
+SoftwareSerial SIM900A(2,3);//TX, RX
 int LED1 = 8;
 int LED2 = 9;
 int FAN = 4;
 int piezo = 5;
 int LED2State = HIGH;
-int temp = 7;
 int x = 0;
-
-
-void setup(){
-  pinMode(2, INPUT);
+void setup()
+{
+  SIM900A.begin(9600);   
+  Serial.begin(9600);    
+  pinMode(7, INPUT);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(piezo, OUTPUT);
   pinMode(FAN, OUTPUT);
-  SIM900A.begin(9600); // Setting the baud rate of GSM Module    
-  Serial.begin(9600); // Setting the baud rate of Serial Monitor (Arduino)   
-  Serial.println ("SIM900A Ready");   
-  delay(1000);   
+  Serial.println ("Starting...");
+  delay(100);
 
 }
 
-void loop(){
-  int gasSensor = digitalRead(2);
+
+void loop()
+{
+   int gasSensor = digitalRead(7);
 
   if(gasSensor == LOW){
-    digitalWrite(piezo, HIGH);
-    digitalWrite(LED1, LOW); 
-    digitalWrite(LED2, HIGH);
-    digitalWrite(FAN, HIGH);
-    if(x%5 == 0){
-      message();
-      delay(5000);
-    }
-    if(x % 10 == 0){
-      call();
-      delay(5000);
-    }
-    if(x == 80){
-      fireStation();
-    }
-    x = x + 1;
-
+      digitalWrite(piezo, HIGH);
+      digitalWrite(LED1, LOW); 
+      digitalWrite(LED2, HIGH);
+      digitalWrite(FAN, HIGH);
+//      x = x + 1;
+//      if(x % 2 == 0){
+       
+//      }
+      
+//      delay(1000);
+      SendMessage();
+      delay(1000);
+      Call();  
+      delay(1000); 
+      fire(); 
+      delay(1000);
+       
   }
   else{
-    digitalWrite(piezo, LOW);
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, LOW);
-    digitalWrite(FAN, LOW);
-    x = 0;
+      digitalWrite(piezo, LOW);
+      digitalWrite(LED1, HIGH);
+      digitalWrite(LED2, LOW);
+      digitalWrite(FAN, LOW);
+      x = 0;
   }
-
+  
   Serial.print("GasSensor = ");
   Serial.print(" ");
   Serial.print(gasSensor);
   Serial.println();
+  
   delay(1000);
-}
 
-void message(){
-     Serial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
-     delay(1000); 
-     Serial.println("AT+CMGS=\"+91xxxxxxxxx\"\r"); // Mobile number
-     Serial.println("Gas Leakage Alert");// The SMS 
-     Serial.println((char)26);
-     delay(8000);
 }
 
 
-void fireStation(){
-     Serial.println("AT+CMGF=1");    //Sets the GSM Module in Text Mode
-     delay(1000); 
-     Serial.println("AT+CMGS=\"+91xxxxxxxxx\"\r"); // Mobile number
-     Serial.println("Fire has been detected. This is a automatic system. Address- your address goes here, you can also take the location from google map. Phone No of the Ownwe - +91xxxxxxxxxx");
-     Serial.println((char)26);
-     delay(8000);
+void SendMessage()   
+  {   
+  Serial.println ("Sending Message");   
+  SIM900A.println("AT+CMGF=1");    
+  delay(2000);   
+  SIM900A.println("AT+CMGS=\"+91xxxxxxxxxx\"\r");   
+  delay(3000);      
+  SIM900A.println("Alert!!! Gas Detected");
+  delay(100);   
+  Serial.println ("Finish");   
+  SIM900A.println((char)26);  
+  delay(1000);   
+  Serial.println ("Message has been sent");   
+  } 
+
+
+void Call()
+{
+    Serial.println("Calling...");
+    delay(2000);
+    SIM900A.println("ATD+91xxxxxxxxxx;");
+    delay(16000);
+    SIM900A.println("ATH");
 }
+
+void fire()   
+  {   
+  Serial.println ("FireStation Message");   
+  SIM900A.println("AT+CMGF=1");    
+  delay(2000);   
+  SIM900A.println("AT+CMGS=\"+91xxxxxxxxxx\"\r");   
+  delay(4000);      
+  SIM900A.println("Alert!!! Fire Detected. Address: Sector V, Saltlake Electronics Complex.  This is a automated gas detection system. Please do the needful");   
+  delay(100);   
+  Serial.println ("Done");   
+  SIM900A.println((char)26);  
+  delay(1000);   
+  Serial.println ("Firestation informed");   
+  }
